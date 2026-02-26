569ea1746263d9ecec73047d79c1c814a2de6524
Alejandro <rodriguez.arale24@triana.salesianos.edu>
2026-02-26 09:40:31 +0100

    fix: arreglado problema de visualizacion de pantalla y arreglos menores

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:trailquest_mobile/features/welcome_page/ui/welcome_page_view.dart';
-import 'package:trailquest_mobile/core/services/token_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trailquest_mobile/features/route_create/route_create_screen.dart';
+import 'package:trailquest_mobile/core/services/route_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  final tokenService = TokenService();
-  final token = await tokenService.getToken();
-  final username = await tokenService.getUsername();
-  
-  if (token != null) {
-    print('TOKEN ENCONTRADO:');
-    print('   Username: $username');
-    print('   Token: ${token.substring(0, 20)}...');
-  } else {
-    print('No hay token guardado (usuario no autenticado)');
-  }
-  
+
   runApp(const MyApp());
 }
 
@@ -25,13 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: 'TrailQuest',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1B512D)),
-        useMaterial3: true,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<RouteService>(
+          create: (context) => RouteService(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TrailQuest',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1B512D)),
+          useMaterial3: true,
+        ),
+        home: const RouteCreateScreen(),
       ),
-      home: const WelcomePageView(),
     );
   }
 }
